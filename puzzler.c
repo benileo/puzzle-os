245dc2390f516bac1df3abf8599572b9e4b21cdf
Tree Man <jammin.irving@gmail.com>
2014-02-14 20:40:43 -0800

    Seg Faults still. Runs to the end of the list without finding the piece

--- puzzler.c
+++ puzzler.c
@@ -8,6 +8,7 @@
 #define DEBUG
 
 void * puzzle_solver( direction dir );
+void find_piece(int row, int column);
 
 int main(int argc, char *argv[])
 {
@@ -24,10 +25,11 @@ int main(int argc, char *argv[])
   printf("Number of threads to solve the problem: %d\n" , thread_count);
 #endif
 
-  /* INITIALIZE GLOBAL DOUBLY LINKED LIST */
+  /* INITIALIZE THE FIRST NODE IN THE LIST */
   head.next = NULL;
   head.prev = NULL;
-  sem_init(&head.node_lock,0,1); //leave the start of the list unlocked
+  head.n=-1;head.e=-1;head.w=-1;head.s=-1;
+  sem_init(&head.node_lock,0,1);
 
   /* GLOBAL GRID */
   grid.cells = NULL; //the pointer is null, not too sure on this one...
@@ -48,8 +50,7 @@ int main(int argc, char *argv[])
   // Solve the problem without using threads
   if (thread_count == 0)
   {
-    //direction dir = SE;
-    //puzzle_solver( (void*)dir );
+      puzzle_solver( SE );
   }
   else
   {
@@ -57,85 +58,152 @@ int main(int argc, char *argv[])
     pthread_attr_init(&atr);
     pthread_attr_setscope( &atr , PTHREAD_SCOPE_SYSTEM );
     pthread_t tpool[thread_count];
-
-
-
   }
 
 
-  //print_grid();
-  //release_memory();
+  print_grid();
+  release_memory();
 
   return 0;
 }
 
-  
 void * puzzle_solver(direction dir)
 {
-    int s_row, s_column;
+  int s_row, s_column;
 
-    switch(dir)
+  switch(dir)
+  {
+    case SE:
+    s_row=0;s_column=0;
+    break;
+    case SW:
+    s_row=0;s_column=grid.numcols-1;
+    break;
+    case NW:
+    s_row=grid.numrows-1;s_column=grid.numcols-1;
+    break;
+    case NE:
+    s_row=grid.numrows-1;s_column=0;
+    break;
+  }
+
+  while
+    (
+     ( (dir == SE) && (s_row < grid.numrows) ) ||
+     ( (dir == SW) && (s_row < grid.numrows) ) ||
+     ( (dir == NW) && (s_row >= 0) ) ||
+     ( (dir == NE) && (s_row >= 0) ) 
+     )
+  {
+
+    //if you can get the lock and the piece is not found
+    if ( 
+     ( sem_trywait( &(grid.cells[s_column][s_row].cell_lock) ) == 0 ) &&
+     ( grid.cells[s_column][s_row].n == -1 || grid.cells[s_column][s_row].e == -1 || 
+       grid.cells[s_column][s_row].w == -1 || grid.cells[s_column][s_row].s == -1 )
+     )
     {
-      case SE:
-        s_row=0;s_column=0;
+      find_piece( s_row , s_column );
+      switch(dir)
+      {
+        case SE:
+          if(s_column == grid.numcols-1){
+            s_row++;s_column=0;
+          }else{
+            s_column++;
+          }
         break;
-      case SW:
-        s_row=0;s_column=grid.numcols-1;
-        break;
-      case NW:
-        s_row=grid.numrows-1;s_column=grid.numcols-1;
-        break;
-      case NE:
-        s_row=grid.numrows-1;s_column=0;
-        break;
-    }
 
-    while
-      (
-       ( (dir == SE) && (s_row < grid.numrows) ) ||
-       ( (dir == SW) && (s_row < grid.numrows) ) ||
-       ( (dir == NW) && (s_row >= 0) ) ||
-       ( (dir == NE) && (s_row >= 0) ) 
-      )
+        case SW:
+          if(s_column == 0){
+            s_row++;s_column=grid.numcols-1;
+          }else{
+            s_column--;
+          }
+          break;
+
+        case NW:
+          if(s_column == 0){
+            s_row--;s_column=grid.numcols-1;
+          }else{
+            s_column--;
+          }
+          break;
+
+        case NE:
+          if(s_column == grid.numcols-1){
+            s_row--;s_column=0;
+          }else{
+            s_column++;
+          }
+          break;
+      }
+    }
+    else
     {
-      //if you can get the lock and the piece is not found
-      if ( 
-	  ( sem_trywait( &(grid.cells[s_column][s_row].cell_lock) ) == 0 ) &&
-	  ( grid.cells[s_column][s_row].n == -1 || grid.cells[s_column][s_row].e == -1 || 
-	    grid.cells[s_column][s_row].w == -1 || grid.cells[s_column][s_row].s == -1 )
-	 )
-	{
-	  find_piece( s_row , s_column);
-	  //increment s_row && column based on direction
-	}
-      else
-	{
-	  //increment
-	}
+      switch(dir)
+      {
+        case SE:
+        s_row++;s_column=0;
+        break;
+        case SW:
+        s_row++;s_column=grid.numcols-1;
+        break;
+        case NW:
+        s_row--;s_column=grid.numcols-1;
+        break;
+        case NE:
+        s_row--;s_column=0;
+        break;
+      }
     }
+  }  
 
-    return NULL;
+
+  return NULL;
 }
 
-  
+
 void find_piece(int row, int column)
 {
   int found=0;
-  node_t *runner;
+  node_t *runner=head.next;
 
-  sem_wait( &(head.next.lock) );
-  runner=head.next;
+  //Get the lock to the head
+  sem_wait( &(head.node_lock) );
 
+  //find the piece 
   while( !found ) 
   {
-    if( grid.cells[column][row].
-
-
+    sem_wait( &(runner->node_lock));
+    if( 
+      (grid.cells[column][row].n == runner->n && grid.cells[column][row].e == runner->e) ||
+      (grid.cells[column][row].n == runner->n && grid.cells[column][row].w == runner->w) ||
+      (grid.cells[column][row].s == runner->s && grid.cells[column][row].e == runner->e) ||
+      (grid.cells[column][row].s == runner->s && grid.cells[column][row].w == runner->w)
+      )
+    {
+      found=1;
+    }
+    else{
+      sem_post( &(runner->prev->node_lock) );
+      runner=runner->next;
+    }
   }
-  
 
+  // Fill in the piece to the grid
+  grid.cells[column][row].n = runner->n;
+  grid.cells[column][row].w = runner->w;
+  grid.cells[column][row].e = runner->e;
+  grid.cells[column][row].s = runner->s;
+  strcpy( grid.cells[column][row].name, runner->name );
+  printf("%s\n", runner->name);
 
+  //Unlink and free the item from the list
+  runner->prev->next = runner->next;
+  sem_post( &(runner->prev->node_lock) );
+  free(runner);
 }
 
-   
-  
+
+
